2f17e2738171d422187046cadb913b94d5757f08
Sandeepan Bhattacharyya <42271103+bsandeepan95@users.noreply.github.com>
2018-12-27 15:25:51 +0530

    updated top documentation

--- ttt_main.c
+++ ttt_main.c
@@ -3,27 +3,16 @@
  * Date: 08-Dec-2018
 
  * Notes:
-    1.  IMPORTANT!! Can you make this playable in PVP and PVC?
-        (Note: Read on Minimax algorithm for PvC. Read these -
-            https://medium.freecodecamp.org/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37
-            https://en.wikipedia.org/wiki/Minimax)
-    2.  Can we implement a better draw checking code that does
-         not need a counter for filled boxes?
-    3.  ERROR: user giving char/str input for box number can cause segmentation error.
-        Maybe try to not implement scanf at all? use fgets with strtol??
+    1.  This file is all you need to add to your project directory.
+    2.  Some code is not compaticle with C89 (ANSI C) standards like int declaration in for loops.
+    
 
- * New Modifications:
+ * Edit Log:
     1.  Initial code is complete to deploy. (15-Dec-2018)
     2.  Successfully(?) implemented logic to run the game multiple
         times based on user's choice. (17-Dec-2018)
-
- * To add:
-    1.  Implement ttt_list as a 2d array. Try to think how to
-        make it work for 5x5 and 7x7 boards. (Note: Making
-        the board names relevant as 00, 01, 02,... RowColumn
-        may prove beneficial. Use string.h for ease maybe?)
-    2.  New code for 5x5 and 7x7 print_board function.
-    3.
+    3.  ERROR: user giving char/str input for box number can cause segmentation error.
+        Maybe try to not implement scanf at all? use fgets with strtol??
 
 */
 
